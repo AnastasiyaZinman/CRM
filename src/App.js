e7dc9dc171c77a66948e5b88d63a04b18af0dd2e
AGCH <anastasiyache@gmail.com>
2018-10-03 14:04:30 +0300

    changing client info on clients page

--- src/App.js
+++ src/App.js
@@ -24,7 +24,35 @@ class App extends Component {
     this.setState({ clients: myJson })
   }
   updateClientDetails =(id,name,surname,country) =>{
-    console.log("AAAApppppp",id,' ',name,' ',surname, " ",country);
+  //console.log("AAAApppppp",id,' ',name,' ',surname, " ",country);
+  // const clientItem = this.findClientItem(id);
+  const clientIndex = this.findClientIndex(id);
+  // console.log("clientItem",clientItem);
+  // console.log("clientIndex",clientIndex);
+  let newState=[...this.state.clients];
+  let clientItem = newState[clientIndex];
+  clientItem = this.changeDataItem(clientItem,name,surname,country);
+  newState[clientIndex]=clientItem;
+  this.setState({clients: newState});
+  console.log("NEW LINE",this.state.clients[clientIndex])
+
+  }
+  changeDataItem(clientItem, n, s, c){
+  clientItem["name"] = n + ' ' + s;
+  clientItem["country"] = c;
+  return clientItem;
+  }
+  findClientItem(id){
+   return this.state.clients.filter(
+      client => client._id==id
+    )[0]
+  }
+  findClientIndex(id){
+    var index;
+    this.state.clients.forEach((f,i)=> { 
+         if (f['_id'] === id) index=i;
+      })
+    return index;
   }
   render() {
     console.log("Clients:", this.state.clients);
