58748b8a0d9ac5d5fe71c2fafbbdafffeb7799b5
AGCH <anastasiyache@gmail.com>
2018-10-04 12:14:21 +0300

    fixes Set for options in Actions class

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -4,29 +4,28 @@ class Actions extends Component {
     constructor() {
         super();
         this.state = {
-            clientsNames : [],
-            ownersNames : []
+            // clientsNames : [],
+            // ownersNames : []
         }
     }
-    generateClientsNames(){
+    generateOptionsSet(property){
         console.log("this.props.clients",this.props.clients);
-        var clientsNames = new Set();
+        let dataArr=[];
+        var clientsNames = new Set(dataArr);
         this.props.clients.forEach(element => {
-            let nameAndId={"name":element['name'],"id":element["_id"]};
-            clientsNames.add(nameAndId);
+            clientsNames.add(element[property]);
         }); 
-        let dataArr = Array.from(clientsNames);
-        console.log("clientsNames",dataArr);
+        dataArr=[...clientsNames]
+        console.log("clientsNames",clientsNames);
         return this.generateOptions(dataArr);
-        
     }
     generateOptions (data){
         console.log("data",data);
         return data.map((item,i) =>{
-                    return (<option 
-                        value={item.id}
-                        key={i++}>{item.name}
-                       </option>)
+            return (<option 
+            value={item}
+            key={i}>{item}
+            </option>)
             })
     }
 
@@ -34,33 +33,37 @@ class Actions extends Component {
     return (
         <div className="action-box">
          <h3>UPDATE</h3>
-         <div className="col  ">Client:  
-         <select>
-             {this.generateClientsNames()}
+         <div className="col">Client:  
+         <select className="ml-3">
+             {this.generateOptionsSet("name")}
         </select>
          </div>
          
         <div className="row action-box">
-            <div className="col-md-3  ">
+            <div className="col-md-2  ">
              Transfer owndership to
              </div>
-             <div className=" col-md-1">
-            <select />
+             <div className=" col-md-2">
+             <select>
+             {this.generateOptionsSet("owner")}
+            </select>
             </div>
             <div className="  col-md-1">
             <button type="button">transfer</button>
             </div>
              <div className="w-100 mt-2"></div>
-             <div className="col-md-3">Send email:</div>
-             <div className="col-md-1">
-             <select />
+             <div className="col-md-2">Send email:</div>
+             <div className="col-md-2">
+             <select>
+             {this.generateOptionsSet("emailType")}
+            </select>
              </div>
              <div className="col-md-1">
              <button type="button">send</button>
              </div>
              <div className="w-100 mt-2"></div>
-             <div className="col-md-3  ">declare sale!</div>
-             <div className="col-md-1">
+             <div className="col-md-2  ">declare sale!</div>
+             <div className="col-md-2">
              </div>
              <div className="col-md-1">
              <button type="button">declare</button>
@@ -70,28 +73,28 @@ class Actions extends Component {
         <hr/>
         <h3>ADD CLIENT</h3>
         <div className="row action-box">
-            <div className="col-md-3  ">
+            <div className="col-md-2  ">
             First name:
              </div>
              <div className="col-md-1">
             <input type="text" />
             </div>
              <div className="w-100 mt-2"></div>
-             <div className="col-md-3  ">
+             <div className="col-md-2  ">
             Surname name:
              </div>
              <div className="  col-md-1">
             <input type="text" />
             </div>
             <div className="w-100 mt-2"></div>
-             <div className="col-md-3  ">
+             <div className="col-md-2  ">
             Country name:
              </div>
              <div className=" col-md-1">
             <input type="text" />
             </div>
             <div className="w-100 mt-2"></div>
-             <div className="col-md-3  ">
+             <div className="col-md-2  ">
             Owner name:
              </div>
              <div className="  col-md-1">
